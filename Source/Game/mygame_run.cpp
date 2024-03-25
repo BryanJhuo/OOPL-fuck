b99567eefb53503c1f3dbc0c08c661eaf1e7d55f
eva930115 <eva93015@gmail.com>
2024-03-26 03:09:19 +0800

    overlap updata

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -86,8 +86,7 @@ void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
 void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
 {
 	if (nFlags == VK_LBUTTON) {
-		TestOverlap(point.x, point.y);
-		//IsMouseOverlap(point.x, point.y);
+		IsMouseOverlap(point.x, point.y);
 		
 	}
 	
@@ -127,6 +126,11 @@ void CGameStateRun::OnShow()
 
 		fireman.character.ShowBitmap();		//(38, 877)
 		watergirl.character.ShowBitmap();	//(38, 737)
+
+		if (CMovingBitmap::IsOverlap(mapButton.mapButton[0], fireman.character)) {	//test isoverlap
+			mapButton.mapButton[0].SetFrameIndexOfBitmap(1);
+		}
+		else mapButton.mapButton[0].SetFrameIndexOfBitmap(0);
 	}
 
 	//Map2
@@ -256,12 +260,3 @@ void CGameStateRun::IsMouseOverlap(int mouse_x, int mouse_y) {
 
 }
 
-
-void CGameStateRun::TestOverlap(int mouse_x, int mouse_y) {
-	if (mouse_x <= 400) {
-		page_phase--;
-	}
-	if (mouse_x >= 400) {
-		page_phase++;
-	}
-}
